80719c34a890e7c822edf067e7fb058d1b8e329d
黄雅婷 <861522731@qq.com>
2020-08-12 16:10:05 +0800

    origin Project

--- src/main/java/com/example/demo/SpringbootHelloApplication.java
+++ src/main/java/com/example/demo/SpringbootHelloApplication.java
@@ -1,24 +1,10 @@
 package com.example.demo;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.retry.annotation.EnableRetry;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableRetry
-@EnableSwagger2
-@MapperScan("com.example.demo.dao")
 public class SpringbootHelloApplication extends SpringBootServletInitializer{
 
 
@@ -26,26 +12,4 @@ public class SpringbootHelloApplication extends SpringBootServletInitializer{
 		SpringApplication.run(SpringbootHelloApplication.class, args);
 	}
 
-	@Bean
-	public Docket createRestApi(){
-		return new Docket(DocumentationType.SWAGGER_2)
-				.apiInfo(apiInfo())
-				.select()
-				.apis(RequestHandlerSelectors.basePackage("com.example.demo.controller"))
-				.paths(PathSelectors.any())
-				.build();
-	}
-
-	private ApiInfo apiInfo(){
-				//页面标题
-				return new ApiInfoBuilder()
-                .title("API测试")
-				//创建人
-				.contact(new Contact("Tracy", "http://www.api.test/", ""))
-				//版本号
-				.version("1.0")
-				//描述
-				.description("API 描述")
-				.build();
-	}
 }
--- src/test/java/com/example/demo/SpringbootHelloApplicationTests.java
+++ src/test/java/com/example/demo/SpringbootHelloApplicationTests.java
@@ -1,39 +1,14 @@
 package com.example.demo;
 
-import com.example.demo.dao.PersonDao;
-import com.example.demo.entity.PageRequest;
-import com.example.demo.entity.PageResult;
-import com.example.demo.entity.Person;
-import com.example.demo.service.PayService;
-import com.example.demo.service.PersonService;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.List;
 
 @SpringBootTest
 class SpringbootHelloApplicationTests {
 
-	@Autowired
-	private PayService payService;
-
-	@Autowired
-	private PersonService personService;
-
 	@Test
-	public void payTest(){
-		payService.minGoodsnumSum();
-	}
+	public void Test(){
 
-	@Test
-	public void personTest(){
-		PageResult page = personService.findPage(new PageRequest(2, 2));
-
-		List<Person> content = (List<Person>) page.getContent();
-		for (Person person:content) {
-			System.out.println(person.getName());
-		}
-		//System.out.println(content.size());
 	}
 }

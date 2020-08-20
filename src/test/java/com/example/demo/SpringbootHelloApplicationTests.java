4cbf573bc9f7a441e6eb80d0d5d9a09210402ad5
黄雅婷 <861522731@qq.com>
2020-08-20 11:25:39 +0800

    jenkins集成allure单元测试报告

--- src/test/java/com/example/demo/SpringbootHelloApplicationTests.java
+++ src/test/java/com/example/demo/SpringbootHelloApplicationTests.java
@@ -2,13 +2,33 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import ru.yandex.qatools.allure.annotations.Features;
+import ru.yandex.qatools.allure.annotations.Stories;
+import ru.yandex.qatools.allure.annotations.Title;
 
 
 @SpringBootTest
 class SpringbootHelloApplicationTests {
+    @Features("测试目录")
+    @Stories("测试子目录")
+    @Title("测试一")
+    @Test
+    public void Test(){
 
-	@Test
-	public void Test(){
+        int a = 9;
+        int b = 3;
+        assert (a+b == 11);
+        // LOGGER.info("jenkins单元测试");
+    }
+    @Features("测试目录")
+    @Stories("测试子目录")
+    @Title("测试二")
+    @Test
+    public void Test2(){
 
-	}
+        int a = 9;
+        int b = 3;
+        assert (a+b == 12);
+        // LOGGER.info("jenkins单元测试");
+    }
 }

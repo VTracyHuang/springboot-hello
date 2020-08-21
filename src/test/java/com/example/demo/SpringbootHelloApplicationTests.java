64f2466955fceacde97335dabdeaa644e2143186
黄雅婷 <861522731@qq.com>
2020-08-21 14:16:23 +0800

    jenkins单元测试

--- src/test/java/com/example/demo/SpringbootHelloApplicationTests.java
+++ src/test/java/com/example/demo/SpringbootHelloApplicationTests.java
@@ -1,7 +1,8 @@
 package com.example.demo;
 
-import org.junit.jupiter.api.Test;
+
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testng.annotations.Test;
 import ru.yandex.qatools.allure.annotations.Features;
 import ru.yandex.qatools.allure.annotations.Stories;
 import ru.yandex.qatools.allure.annotations.Title;
